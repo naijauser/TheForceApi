74bb91db4e88fe5aad459a93e27c838fa4586700
naijauser <unix1gl@gmail.com>
2020-09-12 14:35:05 +0100

    disable db connection

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const ExpressLoader = require('./loaders/Express');
 const connection = require('./config/database');
 
-connection.connect((err) => {
-  if (err) {
-    console.log(`Error connecting to the database: ${err}`);
-    return;
-  }
+// connection.connect((err) => {
+//   if (err) {
+//     console.log(`Error connecting to the database: ${err}`);
+//     return;
+//   }
 
-  console.log(`Database connected: ${connection.threadId}`);
-  const ExpressLoader = require('./loaders/Express');
+//   console.log(`Database connected: ${connection.threadId}`);
+//   const ExpressLoader = require('./loaders/Express');
+//   new ExpressLoader();
+// });
+
+const ExpressLoader = require('./loaders/Express');
   new ExpressLoader();
-});
